d300f841a684e46443ffc31e41e6cda4e50904c1
Tristonev <tristonvarela@gmail.com>
2023-10-17 15:43:34 -0500

    Initial commit

---
+++ app/src/main/java/edu/utsa/cs3443/photoshopwizards/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.utsa.cs3443.photoshopwizards;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println("Hello");
+    }
+}
