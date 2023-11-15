8f9c727eff5f85b715797ff40047c5241dd56a19
Tristonev <tristonvarela@gmail.com>
2023-11-14 18:22:41 -0600

    Adding in new pictures

--- app/src/main/java/edu/utsa/cs3443/photoshopwizards/EditImageActivity.java
+++ app/src/main/java/edu/utsa/cs3443/photoshopwizards/EditImageActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditImageActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageView mainImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -18,6 +20,9 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
 
         int[] buttonIDs = {R.id.EditImageBack, R.id.FlipHorizontal, R.id.FlipVertical, R.id.InvertColor, R.id.Grayscale};
         setupButtons(buttonIDs);
+
+        mainImage = findViewById(R.id.EditingImage);
+        mainImage.setImageResource(R.drawable.dog_canvas);
 
     }
 
@@ -35,6 +40,15 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
         if (view.getId() == R.id.EditImageBack){
             Intent intent = new Intent(this, LayersActivity.class);
             startActivity(intent);
+        }
+
+        if (view.getId() == R.id.FlipHorizontal)
+        {
+            mainImage.setImageResource(R.drawable.dog1_flipped_horizontal);
+        }
+        if (view.getId() == R.id.Grayscale)
+        {
+            mainImage.setImageResource(R.drawable.dog1_grayscale);
         }
 
     }
